c0581175b8babae606c3b257b225a7f7a38a3850
Ten Dmitriy <dima.ten031@gmail.com>
2023-12-05 16:49:02 +0600

    New Tests files

---
+++ sample.py
@@ -0,0 +1,48 @@
+from f1dataparser import *
+import prettytable as pt
+
+parser = FormulaOneParser()
+table = pt.PrettyTable()
+
+table.title = 'Formula One 2023 Season'
+
+drivers_data = parser.parse_drivers_data(2023)
+table.field_names = ['Position', 'Name', 'Nationality', 'Car', 'Points']
+
+
+for driver in drivers_data:
+    table.add_row([driver['position'], driver['name'], driver['nationality'], driver['car'], driver['points']])
+
+print(table)
+table.clear()
+
+races_data = parser.parse_races_data(2023)
+table.field_names = ['Grand Prix', 'Date', 'Winner', 'Car', 'Laps']
+
+for race in races_data:
+    table.add_row([race['grandprix'], race['date'], race['winner'], race['car'], race['laps']])
+    
+print(table)
+table.clear()
+
+teams_data = parser.parse_teams_data(2023)
+
+table.field_names = ['Position', 'Team', 'Points']
+
+
+for team in teams_data:
+    table.add_row([team['position'],team['team'], team['points']])
+    
+print(table)
+table.clear()
+
+fastest_laps_data = parser.parse_fastest_laps_data(2023)
+
+table.field_names = ['Grand Prix', 'Driver', 'Team', 'Lap Time']
+
+
+for lap in fastest_laps_data:
+    table.add_row([lap['grandprix'], lap['driver'], lap['team'], lap['lap_time']])
+    
+print(table)
+table.clear()
---
+++ test_f1dataparser.py
@@ -0,0 +1,52 @@
+import unittest
+from .f1dataparser import *
+
+class FormulaOneParserTests(unittest.TestCase):
+
+    def setUp(self):
+        self.parser = FormulaOneParser()
+
+    def test_parse_drivers_data(self):
+        drivers_data = self.parser.parse_drivers_data(2023)
+        self.assertEqual(len(drivers_data), 22)
+
+        first_driver = drivers_data[0]
+        self.assertEqual(first_driver['position'], '1')
+        self.assertEqual(first_driver['name'], 'Max Verstappen')
+        self.assertEqual(first_driver['nationality'], 'NED')
+        self.assertEqual(first_driver['car'], 'Red Bull Racing Honda RBPT')
+        self.assertEqual(first_driver['points'], '575')
+
+    def test_parse_races_data(self):
+        races_data = self.parser.parse_races_data(2023)
+        self.assertEqual(len(races_data), 22)
+
+        first_race = races_data[0]
+        self.assertEqual(first_race['grandprix'], 'Bahrain')
+        self.assertEqual(first_race['date'], '05 Mar 2023')
+        self.assertEqual(first_race['winner'], 'Max Verstappen')
+        self.assertEqual(first_race['car'], 'Red Bull Racing Honda RBPT')
+        self.assertEqual(first_race['laps'], '57')
+
+    def test_parse_teams_data(self):
+        teams_data = self.parser.parse_teams_data(2023)
+        self.assertEqual(len(teams_data), 10)
+
+        first_team = teams_data[0]
+        self.assertEqual(first_team['position'], '1')
+        self.assertEqual(first_team['team'], 'Red Bull Racing Honda RBPT')
+        self.assertEqual(first_team['points'], '860')
+
+    def test_parse_fastest_laps_data(self):
+        fastest_laps_data = self.parser.parse_fastest_laps_data(2023)
+        self.assertEqual(len(fastest_laps_data), 22)
+
+        first_fastest_lap = fastest_laps_data[0]
+        self.assertEqual(first_fastest_lap['grandprix'], 'Bahrain')
+        self.assertEqual(first_fastest_lap['driver'], 'Zhou')
+        self.assertEqual(first_fastest_lap['team'], 'Alfa Romeo Ferrari')
+        self.assertEqual(first_fastest_lap['lap_time'], '1:33.996')
+
+
+if __name__ == '__main__':
+    unittest.main()
